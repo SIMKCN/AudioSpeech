9dcd6a329ec74d0794fc1a497b09bdcddc3fda29
SIMKCN <skmarschall@gmail.com>
2023-01-22 13:55:31 +0100

    Logging war überflüssig und wurde entfernt (nachfolgende Lösung kommt)

--- SpeechToText.py
+++ SpeechToText.py
@@ -10,17 +10,16 @@ import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import subprocess
-import logging
 import shutil
 
 
 class Audio:
     global get_large_audio_transcription
     global r
-    logging.basicConfig(filename="SpeechToLog.log", filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+
     # create a speech recognition object
     r = sr.Recognizer()
-    logging.info("create a speech recogniton object")
+
     def get_large_audio_transcription(path):
         def start():
             a = 0
@@ -33,7 +32,7 @@ class Audio:
                 # keep the silence for 1 second, adjustable as well
                 keep_silence=500,
             )
-            logging.info("Splited Audio in Chunks")
+            
             
             for i in chunks:
                 a += 1
@@ -42,7 +41,7 @@ class Audio:
             # create a directory to store the audio chunks
             if not os.path.isdir(folder_name):
                 os.mkdir(folder_name)
-                logging.info("Created Folder for Chunks")
+                
                 whole_text = ""
             # process each chunk
                 for i, audio_chunk in enumerate(chunks, start=1):
@@ -50,33 +49,25 @@ class Audio:
                     # the `folder_name` directory
                     chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
                     audio_chunk.export(chunk_filename, format="wav")
-                    logging.info("Exported Chunk ")
                     # recognize the chunk
                     with sr.AudioFile(chunk_filename) as source:
                         audio_listened = r.record(source)
                         # try converting it to text
-                        logging.info("Recognized the Chunk")
                         try: 
                             text = r.recognize_google(audio_listened, language="de-DE")
-                            logging.info("Recognized it with Google")
                         except sr.UnknownValueError as e:
                             print("Error:", str(e))
-                            logging.warning("The chunk could not be rewritten - Chunk is empty ")
                         else:
                             text = f"{text.capitalize()}. "
                             print(chunk_filename, ":", text)
-                            logging.info("Printed chunk text")
                             whole_text += text
                             nonsens = " "
                             datei = open("Transscript.txt", "a")
                             datei.write("\r\n" + chunk_filename + nonsens + text)
                             datei.close()
-                            logging.info("wrote Transcript")
                     root3.update_idletasks()
                     pb['value'] += lengthi
-                    logging.info("Updated Progressbar")
             exit_code = subprocess.call("./Finish.sh")
-            logging.info("Called ./Finish.sh")
 
         root3 = tk.Tk()
         root3.title("Loading...")
@@ -102,7 +93,6 @@ class UI:
     global root
     def datei():
         filename = askopenfilename()
-        logging.info("asked for filename")
         Audio(get_large_audio_transcription(filename))
         
         
@@ -111,15 +101,14 @@ class UI:
         #pb.start()
         
     def anleitung():
-        logging.info("Opened anleitung()")
         root2 = tk.Tk()
         root2.geometry("700x600")
         root2.title("Anleitung")
         frame = tk.Frame(root2)
         frame.pack()
         
-        sb = tk.Scrollbar(frame, orient=HORIZONTAL)
-        sb.pack(fill=X)
+        sb = tk.Scrollbar(frame, orient="horizontal")
+        sb.pack(fill="x")
         lb = tk.Listbox(frame, width=580, height=180, xscrollcommand=sb.set)
         lb.pack()
 
@@ -129,18 +118,18 @@ class UI:
         lb.insert(0, "1.Anleitung")
         lb.insert(1, "2.Vorinformationen")
         lb.insert(2, "3.Achtung! Das Programm funktioniert nur mit .wav Datein!!!")
-        lb.insert(3, "4.Sollten sie andere Audio Formate nutzen könnte es zu Problemen kommen.")
-        lb.insert(4, "5.Das Programm benötigt eine Internetverbindung da es auf eine Datenbank zugreift.")
+        lb.insert(3, "4.Sollten Sie andere Audio Formate nutzen könnte es zu Problemen kommen.")
+        lb.insert(4, "")
         lb.insert(5, "6.Der Transcripter arbeitet nicht 100% perfekt und hat Probleme mit Dialekten.")
         lb.insert(6, "7.Er kann nicht zwischen Stimmen unterscheiden.")
         lb.insert(7, "8.Nach Nutzung sollte der Text nochmal korrigiert werden.")
         lb.insert(8, "9.Dazu werden während des transkribieren Audio Chunks erstellt, jeweils versähen mit dem Namen des Chunks.")
         lb.insert(9, "10.Diese Chunks werden im selben Ordner in der sich auch das Programm befindet gespeichert.")
-        lb.insert(10, "11.Zusätzlich wird der Text in der Datei ""Transscript.txt"" gespeichert welche auch im Ordner des Programms gespeichert wird.")
+        lb.insert(10, "11.Zusätzlich wird der Text in der Datei ""Transscript.txt"" gespeichert, welche auch im Ordner des Programms gespeichert wird.")
         lb.insert(11, "12.Begriffserklärung:")
         lb.insert(12, "Error:Eine Stelle wo keine Sprache erkannt worden ist. (Kann bei langem Atmen o.ä auftreten)")
         lb.insert(13, "-------------------------------------------------------------------")
-        lb.insert(14, "1. Schritt /-/ Drücken sie öffnen und wählen sie die Wav Datei aus!")
+        lb.insert(14, "1. Schritt /-/ Drücken Sie (Öffnen) und wählen sie die Wav Datei aus!")
         lb.insert(15, "2. Ein Fenster öffnet sich mit einem Ladebalken und dem Knopf 'Start'. Drücken sie diesen!")
         lb.insert(16, "Nun fängst das Programm an die Datei in Text um zuschreiben.")
         root2.mainloop()
@@ -149,15 +138,15 @@ class UI:
 
         root.quit()
 
-    logging.info("User logged in")
+
 
     root = tk.Tk()
     root.title("PYTranscriptor")
     root.geometry("200x100")
-    anleitungb = tk.Button(root, text="Anleitung", command=anleitung, fg='#b0d597')
-    close = tk.Button(root, text="Close", command=close, fg='#b0d597')
-    l1 = tk.Label(root, text="Audio Datei:", fg='#b0d597')
-    b1 = tk.Button(root, text="Öffnen", command=datei, fg='#b0d597')
+    anleitungb = tk.Button(root, text="Anleitung", command=anleitung, fg='#000000')
+    close = tk.Button(root, text="Close", command=close, fg='#000000')
+    l1 = tk.Label(root, text="Audio Datei:", fg='#000000')
+    b1 = tk.Button(root, text="Öffnen", command=datei, fg='#000000')
     b1.grid(row=1, column=2, pady=10)
     l1.grid(row=1, column=1, padx=10)
     #pb.grid(row=3, column=1, padx=10, pady=10)
