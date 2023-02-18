c8424b0e0b48330953a2fb2f076cfa61f012220f
SIMKCN <skmarschall@gmail.com>
2023-02-18 23:18:14 +0100

    Ordner ist auch hier

---
+++ source/TransscriptViewer.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+import csv
+def datei():
+    global text1
+    
+    filename = askopenfilename()
+    with open(filename, "r") as file:
+        csv_reader = csv.reader(file)
+        content = list(csv_reader)
+        for row in content:
+            listbox.insert(tk.END, row)
+
+chunkloader = tk.Tk()
+chunkloader.geometry("1000x600")
+scrollbar = tk.Scrollbar(chunkloader)
+scrollbar.pack(side=tk.LEFT, expand=True, fill=tk.Y)
+
+load = tk.Button(chunkloader, text="Open", command=datei)
+listbox = tk.Listbox(chunkloader, height = 1550, width = 300, bg = "grey", activestyle = "dotbox", fg = "black", selectmode="SINGLE")
+
+load.pack(ipadx=10, ipady=10)
+listbox.config(yscrollcommand=scrollbar.set)
+listbox.pack(side=tk.LEFT, expand=True, fill=tk.Y)
+chunkloader.mainloop()
